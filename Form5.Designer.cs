dd464141a56dbe65bdcc40613271c7bbfe04b78e
aysenuressiz <21060631@stu.omu.edu.tr>
2024-01-13 22:13:57 +0300

    proje ekle sayfası eklendi

--- Form5.Designer.cs
+++ Form5.Designer.cs
@@ -28,19 +28,146 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            this.projeAdi = new System.Windows.Forms.TextBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.projeAçiklamasi = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.projeEkle = new System.Windows.Forms.Button();
+            this.geri = new System.Windows.Forms.Button();
+            this.baslangicTarihi = new System.Windows.Forms.DateTimePicker();
+            this.bitisTarihi = new System.Windows.Forms.DateTimePicker();
             this.SuspendLayout();
+            // 
+            // projeAdi
+            // 
+            this.projeAdi.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.projeAdi.Location = new System.Drawing.Point(528, 46);
+            this.projeAdi.Name = "projeAdi";
+            this.projeAdi.Size = new System.Drawing.Size(251, 30);
+            this.projeAdi.TabIndex = 0;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // projeAçiklamasi
+            // 
+            this.projeAçiklamasi.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.projeAçiklamasi.Location = new System.Drawing.Point(528, 119);
+            this.projeAçiklamasi.Name = "projeAçiklamasi";
+            this.projeAçiklamasi.Size = new System.Drawing.Size(251, 30);
+            this.projeAçiklamasi.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(313, 46);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(154, 30);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "PROJE ADI:";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(310, 234);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(157, 30);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "BAŞLANGIÇ TARİHİ:";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // label3
+            // 
+            this.label3.Location = new System.Drawing.Point(310, 320);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(157, 30);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "BİTİŞ TARİHİ:";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // label4
+            // 
+            this.label4.Location = new System.Drawing.Point(310, 119);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(157, 30);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "PROJE AÇIKLAMASI:";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // projeEkle
+            // 
+            this.projeEkle.Location = new System.Drawing.Point(619, 415);
+            this.projeEkle.Name = "projeEkle";
+            this.projeEkle.Size = new System.Drawing.Size(128, 46);
+            this.projeEkle.TabIndex = 11;
+            this.projeEkle.Text = "PROJE EKLE";
+            this.projeEkle.UseVisualStyleBackColor = true;
+            this.projeEkle.Click += new System.EventHandler(this.projeEkle_Click);
+            // 
+            // geri
+            // 
+            this.geri.Location = new System.Drawing.Point(422, 415);
+            this.geri.Name = "geri";
+            this.geri.Size = new System.Drawing.Size(128, 46);
+            this.geri.TabIndex = 12;
+            this.geri.Text = "GERİ";
+            this.geri.UseVisualStyleBackColor = true;
+            this.geri.Click += new System.EventHandler(this.geri_Click);
+            // 
+            // baslangicTarihi
+            // 
+            this.baslangicTarihi.Location = new System.Drawing.Point(528, 234);
+            this.baslangicTarihi.Name = "baslangicTarihi";
+            this.baslangicTarihi.Size = new System.Drawing.Size(251, 22);
+            this.baslangicTarihi.TabIndex = 13;
+            // 
+            // bitisTarihi
+            // 
+            this.bitisTarihi.Location = new System.Drawing.Point(528, 315);
+            this.bitisTarihi.Name = "bitisTarihi";
+            this.bitisTarihi.Size = new System.Drawing.Size(251, 22);
+            this.bitisTarihi.TabIndex = 14;
             // 
             // ProjectAddPage
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1170, 630);
+            this.Controls.Add(this.bitisTarihi);
+            this.Controls.Add(this.baslangicTarihi);
+            this.Controls.Add(this.geri);
+            this.Controls.Add(this.projeEkle);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.projeAçiklamasi);
+            this.Controls.Add(this.projeAdi);
             this.Name = "ProjectAddPage";
             this.Text = "PROJECT ADD PAGE";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox projeAdi;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.TextBox projeAçiklamasi;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button projeEkle;
+        private System.Windows.Forms.Button geri;
+        private System.Windows.Forms.DateTimePicker baslangicTarihi;
+        private System.Windows.Forms.DateTimePicker bitisTarihi;
     }
 }
--- Form5.cs
+++ Form5.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace vtys
 {
@@ -15,6 +16,53 @@ namespace vtys
         public ProjectAddPage()
         {
             InitializeComponent();
+        }
+        static string constring = "Data Source=UNIQUEA-PC\\SQLEXPRESS;Initial Catalog=ProjectTracker;Integrated Security=True";
+        SqlConnection connect = new SqlConnection(constring);
+        private void projeEkle_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // Kullanıcıdan alınan değerleri değişkenlere atayalım
+                string proje_adi = projeAdi.Text;
+                DateTime baslangic_tarihi = baslangicTarihi.Value;
+                DateTime bitis_tarihi = bitisTarihi.Value;
+
+                // Veritabanı bağlantısı oluşturalım
+                string constring = "Data Source=UNIQUEA-PC\\SQLEXPRESS;Initial Catalog=ProjectTracker;Integrated Security=True";
+                using (SqlConnection connect = new SqlConnection(constring))
+                {
+                    // Bağlantıyı açalım
+                    connect.Open();
+
+                    // INSERT sorgusunu oluşturalım ve parametreleri ekleyelim
+                    string sorgu = "INSERT INTO Projeler (Proje_adi, Baslangic_tarihi, Bitis_tarihi) VALUES (@proje_adi, @baslangic_tarihi, @bitis_tarihi)";
+                    using (SqlCommand komut = new SqlCommand(sorgu, connect))
+                    {
+                        komut.Parameters.AddWithValue("@proje_adi", proje_adi);
+                        komut.Parameters.AddWithValue("@baslangic_tarihi", baslangic_tarihi);
+                        komut.Parameters.AddWithValue("@bitis_tarihi", bitis_tarihi);
+
+                        // Sorguyu çalıştıralım
+                        komut.ExecuteNonQuery();
+
+                        // İşlem başarılı mesajını gösterelim
+                        MessageBox.Show("Proje başarıyla eklendi.");
+                    }
+                }
+            }
+            catch (Exception hata)
+            {
+                // Hata durumunda kullanıcıya bilgi verelim
+                MessageBox.Show("Hata meydana geldi!" + hata.Message);
+            }
+        }
+
+        private void geri_Click(object sender, EventArgs e)
+        {
+            HomePage form1 = new HomePage();
+            this.Hide(); // Form2'yi gizle
+            form1.ShowDialog();
         }
     }
 }
