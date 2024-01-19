64a796883cd02fd45526e8851778d2c77bbd841d
Aysegul-Cemc1 <22060661@stu.omu.edu.tr>
2024-01-19 21:36:03 +0300

    9-10 tasarım

--- Form10.Designer.cs
+++ Form10.Designer.cs
@@ -28,20 +28,187 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(AddEmployePage));
+            this.label12 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.mailBox = new System.Windows.Forms.TextBox();
+            this.soyisimBox = new System.Windows.Forms.TextBox();
+            this.isimBox = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.geciciSifreBox = new System.Windows.Forms.TextBox();
+            this.geri = new System.Windows.Forms.Button();
+            this.ekle = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // label12
+            // 
+            this.label12.BackColor = System.Drawing.Color.MidnightBlue;
+            this.label12.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label12.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label12.Location = new System.Drawing.Point(0, 55);
+            this.label12.Name = "label12";
+            this.label12.Padding = new System.Windows.Forms.Padding(70, 12, 12, 12);
+            this.label12.Size = new System.Drawing.Size(300, 45);
+            this.label12.TabIndex = 44;
+            this.label12.Text = "ÇALIŞAN EKLE";
+            // 
+            // label3
+            // 
+            this.label3.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.ForeColor = System.Drawing.Color.MidnightBlue;
+            this.label3.Location = new System.Drawing.Point(440, 263);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(105, 35);
+            this.label3.TabIndex = 49;
+            this.label3.Text = "E-MAİL:";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // label2
+            // 
+            this.label2.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.ForeColor = System.Drawing.Color.MidnightBlue;
+            this.label2.Location = new System.Drawing.Point(415, 203);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(130, 35);
+            this.label2.TabIndex = 48;
+            this.label2.Text = "SOYİSİM:";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.ForeColor = System.Drawing.Color.MidnightBlue;
+            this.label1.Location = new System.Drawing.Point(440, 143);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(105, 35);
+            this.label1.TabIndex = 47;
+            this.label1.Text = "İSİM:";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // mailBox
+            // 
+            this.mailBox.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.mailBox.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.mailBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.mailBox.Location = new System.Drawing.Point(570, 260);
+            this.mailBox.Name = "mailBox";
+            this.mailBox.Size = new System.Drawing.Size(272, 30);
+            this.mailBox.TabIndex = 54;
+            // 
+            // soyisimBox
+            // 
+            this.soyisimBox.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.soyisimBox.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.soyisimBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.soyisimBox.Location = new System.Drawing.Point(570, 200);
+            this.soyisimBox.Name = "soyisimBox";
+            this.soyisimBox.Size = new System.Drawing.Size(272, 30);
+            this.soyisimBox.TabIndex = 53;
+            // 
+            // isimBox
+            // 
+            this.isimBox.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.isimBox.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.isimBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.isimBox.Location = new System.Drawing.Point(570, 140);
+            this.isimBox.Name = "isimBox";
+            this.isimBox.Size = new System.Drawing.Size(272, 30);
+            this.isimBox.TabIndex = 52;
+            // 
+            // label4
+            // 
+            this.label4.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.ForeColor = System.Drawing.Color.MidnightBlue;
+            this.label4.Location = new System.Drawing.Point(380, 323);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(165, 35);
+            this.label4.TabIndex = 55;
+            this.label4.Text = "GEÇİCİ ŞİFRE:";
+            // 
+            // geciciSifreBox
+            // 
+            this.geciciSifreBox.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.geciciSifreBox.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.geciciSifreBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.geciciSifreBox.Location = new System.Drawing.Point(570, 320);
+            this.geciciSifreBox.Name = "geciciSifreBox";
+            this.geciciSifreBox.Size = new System.Drawing.Size(272, 30);
+            this.geciciSifreBox.TabIndex = 56;
+            // 
+            // geri
+            // 
+            this.geri.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.geri.BackColor = System.Drawing.Color.MidnightBlue;
+            this.geri.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.geri.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.geri.Location = new System.Drawing.Point(496, 413);
+            this.geri.Name = "geri";
+            this.geri.Size = new System.Drawing.Size(160, 65);
+            this.geri.TabIndex = 57;
+            this.geri.Text = "GERİ";
+            this.geri.UseVisualStyleBackColor = false;
+            // 
+            // ekle
+            // 
+            this.ekle.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.ekle.BackColor = System.Drawing.Color.MidnightBlue;
+            this.ekle.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.ekle.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.ekle.Location = new System.Drawing.Point(695, 413);
+            this.ekle.Name = "ekle";
+            this.ekle.Size = new System.Drawing.Size(160, 65);
+            this.ekle.TabIndex = 58;
+            this.ekle.Text = "EKLE";
+            this.ekle.UseVisualStyleBackColor = false;
             // 
             // AddEmployePage
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1282, 668);
+            this.Controls.Add(this.ekle);
+            this.Controls.Add(this.geri);
+            this.Controls.Add(this.geciciSifreBox);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.mailBox);
+            this.Controls.Add(this.soyisimBox);
+            this.Controls.Add(this.isimBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label12);
             this.Name = "AddEmployePage";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "ADD EMPLOYEE PAGE";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox mailBox;
+        private System.Windows.Forms.TextBox soyisimBox;
+        private System.Windows.Forms.TextBox isimBox;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox geciciSifreBox;
+        private System.Windows.Forms.Button geri;
+        private System.Windows.Forms.Button ekle;
     }
 }
--- Form9.Designer.cs
+++ Form9.Designer.cs
@@ -32,6 +32,16 @@
             this.geri = new System.Windows.Forms.Button();
             this.guncelle = new System.Windows.Forms.Button();
             this.label12 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.isimBox = new System.Windows.Forms.TextBox();
+            this.soyisimBox = new System.Windows.Forms.TextBox();
+            this.mailBox = new System.Windows.Forms.TextBox();
+            this.sifreBox = new System.Windows.Forms.TextBox();
+            this.telefonBox = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // geri
@@ -40,7 +50,7 @@
             this.geri.BackColor = System.Drawing.Color.MidnightBlue;
             this.geri.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.geri.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.geri.Location = new System.Drawing.Point(195, 342);
+            this.geri.Location = new System.Drawing.Point(495, 470);
             this.geri.Name = "geri";
             this.geri.Size = new System.Drawing.Size(160, 65);
             this.geri.TabIndex = 42;
@@ -54,7 +64,7 @@
             this.guncelle.BackColor = System.Drawing.Color.MidnightBlue;
             this.guncelle.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.guncelle.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.guncelle.Location = new System.Drawing.Point(413, 342);
+            this.guncelle.Location = new System.Drawing.Point(695, 470);
             this.guncelle.Name = "guncelle";
             this.guncelle.Size = new System.Drawing.Size(160, 65);
             this.guncelle.TabIndex = 41;
@@ -74,13 +84,138 @@
             this.label12.TabIndex = 43;
             this.label12.Text = "ÇALIŞAN GÜNCELLE";
             // 
+            // label5
+            // 
+            this.label5.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.label5.BackColor = System.Drawing.Color.Transparent;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label5.ForeColor = System.Drawing.Color.MidnightBlue;
+            this.label5.Location = new System.Drawing.Point(423, 383);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(121, 35);
+            this.label5.TabIndex = 48;
+            this.label5.Text = "TELEFON:";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // label4
+            // 
+            this.label4.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.ForeColor = System.Drawing.Color.MidnightBlue;
+            this.label4.Location = new System.Drawing.Point(441, 323);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(103, 35);
+            this.label4.TabIndex = 47;
+            this.label4.Text = "ŞİFRE:";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // label3
+            // 
+            this.label3.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.ForeColor = System.Drawing.Color.MidnightBlue;
+            this.label3.Location = new System.Drawing.Point(441, 263);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(103, 35);
+            this.label3.TabIndex = 46;
+            this.label3.Text = "E-MAİL:";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // label2
+            // 
+            this.label2.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.ForeColor = System.Drawing.Color.MidnightBlue;
+            this.label2.Location = new System.Drawing.Point(400, 203);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(144, 35);
+            this.label2.TabIndex = 45;
+            this.label2.Text = "SOYİSİM:";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.ForeColor = System.Drawing.Color.MidnightBlue;
+            this.label1.Location = new System.Drawing.Point(441, 143);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(103, 35);
+            this.label1.TabIndex = 44;
+            this.label1.Text = "İSİM:";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // isimBox
+            // 
+            this.isimBox.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.isimBox.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.isimBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.isimBox.Location = new System.Drawing.Point(570, 140);
+            this.isimBox.Name = "isimBox";
+            this.isimBox.Size = new System.Drawing.Size(272, 30);
+            this.isimBox.TabIndex = 49;
+            // 
+            // soyisimBox
+            // 
+            this.soyisimBox.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.soyisimBox.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.soyisimBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.soyisimBox.Location = new System.Drawing.Point(570, 200);
+            this.soyisimBox.Name = "soyisimBox";
+            this.soyisimBox.Size = new System.Drawing.Size(272, 30);
+            this.soyisimBox.TabIndex = 50;
+            // 
+            // mailBox
+            // 
+            this.mailBox.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.mailBox.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.mailBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.mailBox.Location = new System.Drawing.Point(570, 260);
+            this.mailBox.Name = "mailBox";
+            this.mailBox.Size = new System.Drawing.Size(272, 30);
+            this.mailBox.TabIndex = 51;
+            // 
+            // sifreBox
+            // 
+            this.sifreBox.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.sifreBox.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.sifreBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.sifreBox.Location = new System.Drawing.Point(570, 320);
+            this.sifreBox.Name = "sifreBox";
+            this.sifreBox.Size = new System.Drawing.Size(272, 30);
+            this.sifreBox.TabIndex = 52;
+            // 
+            // telefonBox
+            // 
+            this.telefonBox.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.telefonBox.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.telefonBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.telefonBox.Location = new System.Drawing.Point(570, 380);
+            this.telefonBox.Name = "telefonBox";
+            this.telefonBox.Size = new System.Drawing.Size(272, 30);
+            this.telefonBox.TabIndex = 53;
+            // 
             // UpdateEmployeePage
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1282, 668);
+            this.Controls.Add(this.telefonBox);
+            this.Controls.Add(this.sifreBox);
+            this.Controls.Add(this.mailBox);
+            this.Controls.Add(this.soyisimBox);
+            this.Controls.Add(this.isimBox);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.label12);
             this.Controls.Add(this.geri);
             this.Controls.Add(this.guncelle);
@@ -88,6 +223,7 @@
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "UPDATE EMPLOYEE PAGE";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -95,5 +231,15 @@
         private System.Windows.Forms.Button geri;
         private System.Windows.Forms.Button guncelle;
         private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox isimBox;
+        private System.Windows.Forms.TextBox soyisimBox;
+        private System.Windows.Forms.TextBox mailBox;
+        private System.Windows.Forms.TextBox sifreBox;
+        private System.Windows.Forms.TextBox telefonBox;
     }
 }
